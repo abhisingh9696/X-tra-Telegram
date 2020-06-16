7d5dde583cf504a4f1ac452d6981f516e0ffc5da
Abhishek Singh <41541819+abhisingh9696@users.noreply.github.com>
2020-06-16 14:19:53 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Gu
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Currently Alive, "
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
-                     # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                 
-                     f": {DEFAULTUSER}\n\n"
+    await alive.edit(
                     
+                   
+                     f"`MY MASTER`: {DEFAULTUSER}\n\n"
+                    )
